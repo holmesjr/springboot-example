6febe6cceed95b78690b926a3d7f07b73b3d6048
James Holmes <james.holmes@gmail.com>
2019-02-16 20:13:02 +1100

    Auth Endpoint
    Add a simple auth endpoint, with manual, hardcoded config.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.ldap.support.LdapUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import javax.naming.directory.DirContext;
 
 @SpringBootApplication
 @RestController
@@ -37,6 +41,31 @@ public class Application {
 
         return users.toString();
     }
+
+    @RequestMapping("/authenticate")
+    public String authenticate(@RequestParam("username") String username, @RequestParam("password") String password) {
+
+        DirContext ctx = null;
+        LdapContextSource contextSource = new LdapContextSource();
+        contextSource.setUrl("ldap://localhost:8389/");
+        contextSource.setBase("dc=springframework,dc=org");
+        contextSource.afterPropertiesSet();
+        try {
+            // USERS LIKE THIS FOR NOW uid=ben,ou=people,dc=springframework,dc=org
+            ctx = contextSource.getContext(
+                    "uid=" + username + ",ou=people,dc=springframework,dc=org",
+                    password);
+        } catch (Exception e) {
+            // Context creation failed - authentication did not succeed
+            return "DENIED: " + e.toString();
+        } finally {
+            // It is imperative that the created DirContext instance is always closed
+            LdapUtils.closeContext(ctx);
+        }
+
+        return "SUCCEEDED";
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
